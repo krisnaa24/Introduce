f8d1328ef7559f5c324f7ddf7c24100d4bb044ab
krisnaa24 <94034757+krisnaa24@users.noreply.github.com>
2021-11-13 09:49:37 +0700

    Add files via upload

---
+++ github.py
@@ -0,0 +1,9 @@
+nama = 'Nurferdiansyah Krisna Pratama Putra' 
+NIM = 212410102063 
+umur = 19 
+asal = 'Sumenep'
+
+print("Nama :", nama)
+print("NIM :", NIM)
+print("Umur :", umur , "Tahun")
+print("Asal :", asal)
